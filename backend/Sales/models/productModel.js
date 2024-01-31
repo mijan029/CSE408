29a1c5d72b126b80381d6e3ae15634409362453a
Mijanur Rahman <mijanurcsebuet@gmail.com>
2024-02-01 02:52:03 +0600

    First commit for the project. Sales module has been created and worked. Server.js will be the main file for backend.

---
+++ backend/Sales/models/productModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const schema = new Schema(
+    {
+        category:{
+            type: String,
+            required: true
+        },
+        name:{
+            type: String,
+            required: true
+        },
+        price:{
+            type: Number,
+            required: true
+        }
+    },
+
+    {
+        timestamps: true
+    }
+) 
+
+module.exports = new mongoose.model('productModel', schema)
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,25 @@
-require('dotenv').config();
-const express = require('express');
-const router = require('./routes/workouts');
-const mongoose = require('mongoose');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
 
-app.get('/',(req,res)=>{
-    res.send("baler kaj egula");
-});
-app.use(express.json());
-app.use('/api/workoutes', router);
+app.get('/', (req,res)=>{
+    res.send("Home")
+})
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(()=>{
-        app.listen(process.env.PORT,()=>{
-            console.log(`Connected and listen to ${process.env.PORT}`);
-        });
+app.use(express.json())
+
+
+//The following router is for Sales Management
+//app.use('admin/product',)
+
+
+mongoose.connect(process.env.MONGO_URI).then(
+    app.listen(process.env.PORT,()=>{
+        console.log(`Connected to Database and Listen to port ${process.env.PORT}`)
     })
-    .catch(error=>{
-        console.log("Not connected");
-    });
-
-
-    
+).catch(
+    error => {
+        console.log("Can not be Connected")
+    }
+)
